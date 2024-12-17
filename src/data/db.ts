f87342ec09eb33b889c0387856413997dda0051b
Fatorres <ftorres2018@alu.uct.cl>
2024-12-17 12:57:47 -0300

    All Project

---
+++ src/data/db.ts
@@ -0,0 +1,100 @@
+import { Guitar } from "../types/index";
+
+export const db: Guitar[] = [
+  {
+    id: 1,
+    name: "FIGURA DARK SOULS THE NAMELESS KING",
+    image: "Figura-1",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 299,
+  },
+  {
+    id: 2,
+    name: "NAVE STAR WARS V-WING STARFIGHTER-JAZWARES",
+    image: "Figura-2",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 349,
+  },
+  {
+    id: 3,
+    name: "TRANSFORMERS LEGACY ANIMATED UNIVERSE OPTIMUS PRIME",
+    image: "Figura-3",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 329,
+  },
+  {
+    id: 4,
+    name: "FIGURA SANDMAN SPIDERMAN NO WAY HOME",
+    image: "Figura-4",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 299,
+  },
+  {
+    id: 5,
+    name: "DRAGON BALL SUPER SCULPTURES BIG BUDOUKAI SUPER SAIYAN 2 SON GOKOU (13CM) (GOKU)",
+    image: "Figura-5",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 399,
+  },
+  {
+    id: 6,
+    name: "BANDAI TRANS-AM RAISER HG (1:144)",
+    image: "Figura-6",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 329,
+  },
+  {
+    id: 7,
+    name: "FIGURA MARVEL LEGENDS NED LEEDS AND PETER PARKER",
+    image: "Figura-7",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 349,
+  },
+  {
+    id: 8,
+    name: "ONE PIECE SCULTURES BIG ZOUKEIO 5 UROUGE (11CM)",
+    image: "Figura-8",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 379,
+  },
+  {
+    id: 9,
+    name: "MARVEL´S PUMA MARVEL LEGENDS SERIES SPIDER MAN",
+    image: "Figura-9",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 289,
+  },
+  {
+    id: 10,
+    name: "MARVEL LEGENDS SERIES DEADPOOL CORPS MARVEL 80 YEARS",
+    image: "Figura-10",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 349,
+  },
+  {
+    id: 11,
+    name: "FIGURA POP UP PARADE ASUKA LANGLEY XL SIZE",
+    image: "Figura-11",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 399,
+  },
+  {
+    id: 12,
+    name: "FIGURA ESTREAM SATORU GOJO MONOCHROME VER. 1/7",
+    image: "Figura-12",
+    description:
+      "Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.",
+    price: 379,
+  },
+];
